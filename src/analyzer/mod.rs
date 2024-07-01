eee8303b816cc5b9fdef6ae967ee914f9cac68e9
yui019 <yui36189@gmail.com>
2024-07-01 21:09:01 +0200

    Rename analyzer function
    I'm also going to be verifying variables exist and probably more
    things, so this makes more sense I guess

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -1,5 +1,5 @@
 use crate::parser::expression::Expression;
 
-pub fn type_checking(ast: &mut Vec<Expression>) -> Result<(), String> {
+pub fn validate(ast: &mut Vec<Expression>) -> Result<(), String> {
     return Ok(());
 }
