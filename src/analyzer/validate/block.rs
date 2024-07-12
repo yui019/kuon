3edfce75653230a705bdd75c62e4a9e1acd3202c
yui019 <yui36189@gmail.com>
2024-07-13 00:42:44 +0200

    Fix bug where the last expression in a block would get validated twice

--- src/analyzer/validate/block.rs
+++ src/analyzer/validate/block.rs
@@ -10,12 +10,15 @@ pub fn validate_block(
     expressions: &Vec<Expression>,
 ) -> Result<Type, String> {
     let mut block_env = Environment::from_parent(env);
-    let mut expressions_copy = expressions.clone();
+    let expressions_copy = expressions.clone();
+    let len = expressions_copy.len();
 
-    for mut expression in &mut expressions_copy {
+    let expressions_without_last_expression = &expressions_copy[..len - 1];
+    let last_expression = &expressions_copy[len - 1];
+
+    for mut expression in expressions_without_last_expression {
         validate_and_get_type(&mut expression, &mut block_env)?;
     }
 
-    let last_expression = &expressions_copy[expressions_copy.len() - 1];
     return validate_and_get_type(last_expression, &mut block_env);
 }
